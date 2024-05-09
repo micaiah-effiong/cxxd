ce4192e6e1448106c72904b8142e1041edfee626
Micah Effiong <micaiah.effiong@gmail.com>
2024-05-09 22:13:27 +0100

    revert to 'int' and use 'output_text' as result variable

--- cxxd.c
+++ cxxd.c
@@ -10,20 +10,19 @@ int main(int argc, char *argv[]) {
 
   if (filepath == NULL) {
     printf("Could not resolve file path\n");
-    printf("%s\n", filepath);
+    exit(1);
     return 1;
   }
-
-  printf("%s\n", filepath);
 
   FILE *file = fopen(filepath, "r");
 
   if (!file) {
     printf("could not get file");
+    exit(1);
     return 1;
   }
 
-  int64_t length;
+  int length;
 
   fseek(file, 0, SEEK_END);
   length = ftell(file);
@@ -31,37 +30,32 @@ int main(int argc, char *argv[]) {
   fseek(file, 0, SEEK_SET);
 
   // char text[length];
-  int64_t hex_buffer[length];
-  int64_t s;
-  int64_t hex_index = 0;
-  while ((s = fgetc(file)) != EOF) {
-    hex_buffer[hex_index] = s;
+  int hex_buffer[length];
+  int chunk;
+  int hex_index = 0;
+  while ((chunk = fgetc(file)) != EOF) {
+    hex_buffer[hex_index] = chunk;
     hex_index++;
   }
 
-  // printf("length: %d\nhex length: %d\n", length, (int)sizeof(hex_buffer));
-
   fclose(file);
 
-  int64_t offset = 0;
-  int64_t offset_size = 16;
-  for (int i = 0; i < length; i += offset_size) {
-    // printf(">> %#08x %d\n", offset, hex_buffer[i]);
-    int64_t offset_end = min(offset + offset_size, length);
+  int offset = 0;
+  int offset_size = 16;
+  char output_text[length + 100]; // buffer_length + 100(meta data)
+  sprintf(output_text + strlen(output_text),
+          "filepath: %s\nlength: %d bytes\n\n", filepath, length);
 
-    // int chunk_size = offset_end - offset;
-    // printf("chunk size: %d\n", offset_end - offset);
-    // int chuck[chunk_size];
+  for (int i = 0; i < length; i += offset_size) {
+    int offset_end = min(offset + offset_size, length);
 
     // 8 + 43 + 16
     char text[100];
     char output[100];
-    // int space_count = 0;
-    for (int64_t j = offset; j < offset_end; j++) {
-      // printf(">count %d %d %d\n", j, offset_end, length);
 
-      int64_t val = hex_buffer[j];
-      int64_t text_pos = j - offset;
+    for (int j = offset; j < offset_end; j++) {
+      int val = hex_buffer[j];
+      int text_pos = j - offset;
 
       if (val > 31 && val < 128) {
         text[text_pos] = val;
@@ -69,27 +63,22 @@ int main(int argc, char *argv[]) {
         text[text_pos] = 46;
       }
 
-      // printf("%d %c %d\n", val, text[text_pos], j);
-
-      // printf("%d %c\n", hex_buffer[j], (char)hex_buffer[j]);
       sprintf(output + strlen(output), "%02x", (char)val);
-      // sprintf(&output[strlen(output)], " ");
-      //
+      // printf("j: %d text: %s\n", j, output);
+
       if (j % 2 != 0) {
         strcat(output, " ");
       }
-      // printf("size: %d \n", sizeof(output));
-      // space_count++;
     }
 
-    int64_t chunk_size = offset_end - offset;
+    int chunk_size = offset_end - offset;
     // printf("chunc size %d\n", chunk_size);
 
     if (chunk_size % offset_size != 0) {
-      int64_t diff = offset_size - chunk_size;
+      int diff = offset_size - chunk_size;
 
-      for (int64_t i = 0; i < diff; i++) {
-        sprintf(text + strlen(text), "%c", '.');
+      for (int i = 0; i < diff; i++) {
+        // sprintf(text + strlen(text), "%c", '.');
         sprintf(output + strlen(output), "%s", "  ");
 
         if ((diff - i) % 2 != 0) {
@@ -98,13 +87,15 @@ int main(int argc, char *argv[]) {
       }
     }
 
-    printf("%08x: %s %s\n", offset, output, text);
-    // printf(">text: %s\n", text);
+    sprintf(output_text + strlen(output_text), "%08x: %s %s\n", (int)offset,
+            output, text);
 
     memset(output, 0, sizeof(output));
     memset(text, 0, sizeof(text));
     offset += 16;
   }
+
+  printf("%s", output_text);
 
   return 0;
 }
