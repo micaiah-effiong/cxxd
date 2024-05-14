650ca3d77ab783b3bdd362ad3174d650b44ece67
Micah Effiong <micaiah.effiong@gmail.com>
2024-05-15 00:02:53 +0100

    refactor(main): added read_file()
    - update hex_dump() to read file using read_file()

--- cxxd.c
+++ cxxd.c
@@ -7,28 +7,41 @@
 int min(int a, int b);
 char *hex_dump(FILE *file);
 
+struct HexFileData {
+  char *data;
+  int length;
+};
+struct HexFileData read_file(FILE *file);
+
 int main(int argc, char *argv[]) {
   char *argv_filename = argv[1];
 
   int opt;
   FILE *ouput_file = stdout;
-  char *output_filepath = NULL; // = (char *)malloc(PATH_MAX);
+  char *output_filepath = (char *)malloc(PATH_MAX);
 
-  while ((opt = getopt(argc, argv, "o:s:")) != -1) {
+  if (output_filepath == NULL) {
+    printf("Could not allocate memory\n");
+    exit(0);
+    return 1;
+  }
+
+  while ((opt = getopt(argc, argv, "o:")) != -1) {
 
     switch (opt) {
     case 'o':
-      output_filepath = realpath(optarg, NULL);
+
+      // TODO: handle realpath outside loop
+      realpath(optarg, output_filepath);
 
       if (output_filepath == NULL) {
         printf("Could not resolve file path\n");
         free(output_filepath);
+        output_filepath = NULL;
         exit(1);
         return 1;
       }
 
-      ouput_file = fopen(output_filepath, "w");
-      free(output_filepath);
       break;
     default:
       break;
@@ -44,6 +57,7 @@ int main(int argc, char *argv[]) {
 
   FILE *file = fopen(filepath, "r");
   free(filepath);
+  filepath = NULL;
 
   if (!file) {
     printf("could not get file");
@@ -53,31 +67,29 @@ int main(int argc, char *argv[]) {
 
   char *result = hex_dump(file);
 
+  if (output_filepath != NULL && strlen(output_filepath) > 0) {
+    ouput_file = fopen(output_filepath, "w");
+    free(output_filepath);
+    output_filepath = NULL;
+  }
+
   fwrite(result, 1, strlen(result), ouput_file);
   fclose(ouput_file);
+  ouput_file = NULL;
 
   free(result);
+  result = NULL;
 
   return 0;
 }
 
 char *hex_dump(FILE *file) {
-
-  fseek(file, 0, SEEK_END);
-  int length = ftell(file);
-
-  fseek(file, 0, SEEK_SET);
-
-  char *data = (char *)malloc(length);
-  // char data[length];
-  fread(data, 1, length, file);
-  data[length] = '\0';
-
-  fclose(file);
+  struct HexFileData hex_file = read_file(file);
 
   int offset = 0;
   int offset_size = 16;
-  char *output_text = (char *)malloc((length * length) * sizeof(char *));
+  int output_size = (hex_file.length * hex_file.length) * sizeof(char *);
+  char *output_text = (char *)malloc(output_size);
 
   if (output_text == NULL) {
     printf("Could not allocate memory for");
@@ -90,15 +102,15 @@ char *hex_dump(FILE *file) {
   /* sprintf(output_text + strlen(output_text),
           "filepath: %s\nlength: %d bytes\n\n", filepath, length); */
 
-  for (int i = 0; i < length; i += offset_size) {
-    int offset_end = min(offset + offset_size, length);
+  for (int i = 0; i < hex_file.length; i += offset_size) {
+    int offset_end = min(offset + offset_size, hex_file.length);
 
     // 8 + 43 + 16
     char text[17];
     char output[44];
 
     for (int j = offset; j < offset_end; j++) {
-      int val = data[j];
+      int val = hex_file.data[j];
       int text_pos = j - offset;
 
       if (val >= 32 && val <= 126) {
@@ -144,8 +156,26 @@ char *hex_dump(FILE *file) {
   }
 
   sprintf(output_text + strlen(output_text), "%c", '\0');
+  free(hex_file.data);
+  hex_file.data = NULL;
 
   return output_text;
+}
+
+struct HexFileData read_file(FILE *file) {
+  fseek(file, 0, SEEK_END);
+  int length = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  char *data = (char *)malloc(length);
+
+  fread(data, 1, length, file);
+  data[length] = '\0';
+  fclose(file);
+  // file = NULL;
+
+  struct HexFileData result = {data, length};
+  return result;
 }
 
 int min(int a, int b) {
